4ec57859ec08ecc4a3cb2abecea3789ac4c5a4d8
wire2c7 <harigane.2c7@gmail.com>
2024-10-21 22:18:24 +0900

    [Modified] バッチファイルの変数まわりの記述を変更

--- make-symlink.bat
+++ make-symlink.bat
@@ -1,22 +1,42 @@
 @echo off
 setlocal
-set current_dir=%~dp0
+set bat_file_dir=%~dp0
 
-set link_path=%LOCALAPPDATA%\nvim
-set target_path=%current_dir%
-
-if exist "%link_path%" (
-	echo Link ^("%link_path%"^) already exists
-	exit /b 1
+if defined LOCALAPPDATA (
+  set link_path=%LOCALAPPDATA%\nvim
+) else if defined USERPROFILE (
+  set link_path=%USERPROFILE%\AppData\Local\nvim
+) else if defined USERNAME (
+  set link_path=C:\Users\%USERNAME%\AppData\Local\nvim
 )
 
-set arguments="'/c,mklink,/d,\"%link_path%\",\"%target_path%\"'"
+if not defined link_path (
+  echo "Could not set the path of the symbolic link."
+  call :wait
+  exit /b 127
+) else if exist "%link_path%" (
+  echo "%link_path%" already exists
+  call :wait
+  exit /b 1
+)
+
+echo Make symbolic link to "%link_path%"
+
+set target_path=%bat_file_dir%
+set arguments='/c,mklink,/d,\"%link_path%\",\"%target_path%\"'
 
 powershell ^
-	-Command Start-Process ^
-	-FilePath "cmd" ^
-	-ArgumentList %arguments% ^
-	-Verb Runas
+  -Command Start-Process ^
+  -FilePath "cmd" ^
+  -ArgumentList "%arguments%" ^
+  -Verb Runas
 
-timeout /t 5 /nobreak
+call :wait
+
 endlocal
+exit /b 0
+
+:wait
+  timeout /t 5 /nobreak
+  exit /b 0
+
