fab49a0399b738a88838375c1e736a2360006657
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-15 02:34:46 +0900

    feat: TreeSitterプラグインの追加

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,14 @@
+local M = {
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('vim-treesitter-start', {}),
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end
+}
+
+return { M }
