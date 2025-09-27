40a2de7ebe9db7c1a62aa314a661b8c00b13ac3f
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-27 15:23:47 +0900

    change: LSP設定を移動

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,3 +1,4 @@
+local mapper = require('utils.keymapper')
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
@@ -33,5 +34,82 @@ autocmd({ 'TermOpen' }, {
     opt_local.number = false
     opt_local.relativenumber = false
     cmd.startinsert()
+  end,
+})
+
+-- Language Serverアタッチ
+autocmd({ 'LspAttach' }, {
+  group = augroup('user__lsp_attach', {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    local default_opts = { buffer = ev.buf }
+    local function nmap(lhs, rhs, opts)
+      mapper.n(lhs, rhs, vim.tbl_deep_extend('force', default_opts, opts or {}))
+    end
+    local function imap(lhs, rhs, opts)
+      mapper.i(lhs, rhs, vim.tbl_deep_extend('force', default_opts, opts or {}))
+    end
+
+    -- ホバー情報
+    nmap('K', buf.hover, { desc = '情報のホバー表示' })
+    nmap('<C-k>', buf.signature_help, { desc = 'シグネチャを表示' })
+    imap('<C-k>', buf.signature_help, { desc = 'シグネチャを表示' })
+
+    -- 宣言ジャンプ
+    nmap('gD', buf.declaration, { desc = '宣言へジャンプ' })
+    -- 定義ジャンプ
+    nmap('gd', buf.definition, { desc = '定義へジャンプ' })
+    -- 型定義ジャンプ
+    nmap('gtd', buf.type_definition, { desc = '型定義へジャンプ' })
+    -- 実装ジャンプ
+    nmap('gi', buf.implementation, { desc = '実装へジャンプ' })
+
+    -- フォーマッティング
+    nmap('gf', function()
+      buf.format({ async = true })
+    end, { desc = 'フォーマット' })
+
+    -- コードアクション
+    nmap('ga', buf.code_action, { desc = 'コードアクション表示' })
+    -- リネーム
+    nmap('gn', buf.rename, { desc = 'リネーム' })
+
+    -- 参照
+    nmap('gr', buf.references, { desc = '参照表示' })
+
+    -- ワークスペース
+    nmap('gwa', buf.add_workspace_folder, { desc = '現在のバッファをワークスペースへ追加' })
+    nmap('gwr', buf.remove_workspace_folder, { desc = '現在のバッファをワークスペースから削除' })
+    nmap('gwl', function()
+      print(vim.inspect(buf.list_workspace_folders()))
+    end, { desc = 'ワークスペースリストを表示' })
+
+    local attached_client = lsp.get_client_by_id(ev.data.client_id)
+    if attached_client ~= nil then
+      if attached_client:supports_method('textDocument/formatting') then
+        local tbl = require('lsp.disableformatting')
+
+        -- 保存時フォーマット
+        autocmd({ 'BufWritePre' }, {
+          group = augroup('user__format_on_save', {}),
+          buffer = ev.buf,
+          callback = function()
+            lsp.buf.format({
+              timeout_ms = 3000,
+              async = true,
+              filter = function(client)
+                for _, ls in ipairs(tbl) do
+                  if ls == client.name then
+                    return true
+                  end
+                end
+                return false
+              end,
+            })
+          end,
+        })
+      end
+    end
   end,
 })
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,12 +1,12 @@
 local mapper = require('utils.keymapper')
 
 local lsp = vim.lsp
-
-local buf = lsp.buf
 local diag = vim.diagnostic
 
+-- Set log level
 -- lsp.log.set_level(lsp.log.levels.ERROR)
 
+-- Enable Language Servers
 lsp.enable({
   -- Lua
   'lua_ls',
@@ -20,43 +20,8 @@ lsp.enable({
   'biome',
 })
 
-mapper.nmap('ge', diag.open_float)
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    local opts = { buffer = ev.buf }
-    local function nmap(lhs, rhs)
-      mapper.nmap(lhs, rhs, opts)
-    end
-    local function imap(lhs, rhs)
-      mapper.imap(lhs, rhs, opts)
-    end
-
-    -- ホバー情報
-    nmap('K', buf.hover)
-    -- 定義ジャンプ
-    nmap('gd', buf.definition)
-    -- 型定義ジャンプ
-    nmap('grt', buf.type_definition)
-    -- フォーマッティング
-    nmap('gf', function()
-      buf.format({ async = true })
-    end)
-
-    nmap('<C-k>', buf.signature_help)
-    imap('<C-k>', buf.signature_help)
-
-    nmap('gwa', buf.add_workspace_folder)
-    nmap('gwr', buf.remove_workspace_folder)
-    nmap('gwl', function()
-      print(vim.inspect(buf.list_workspace_folders()))
-    end)
-  end,
-})
-
+-- Diagnostic
+mapper.n('ge', diag.open_float)
 diag.config({
   virtual_lines = true,
 })
