66a0ac78e8bf76a72cc53ac52bd74cd3f6d41891
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-27 15:26:51 +0900

    chore: フォーマッタによるフォーマット

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -103,23 +103,23 @@ o.autoindent = true
 o.smartindent = true
 
 -- タブ入力
-o.expandtab = true  -- タブキー入力をタブ文字の代わりにスペースにする
-o.shiftwidth = 0    -- 0 → tabstopに従う
+o.expandtab = true -- タブキー入力をタブ文字の代わりにスペースにする
+o.shiftwidth = 0 -- 0 → tabstopに従う
 o.smarttab = true
-o.softtabstop = -1  -- 負数 → tabstopに従う
-o.tabstop = 2       -- タブ入力のスペース幅
+o.softtabstop = -1 -- 負数 → tabstopに従う
+o.tabstop = 2 -- タブ入力のスペース幅
 
 --
 if vim.loop.os_uname().sysname == 'Windows_NT' then
   o.keywordprg = ':help'
 
   o.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
-  o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; $PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText; '
+  o.shellcmdflag =
+    '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; $PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText; '
   o.shellquote = ''
   o.shellxquote = ''
   o.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
   o.shellpipe = '2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode'
-
 
   vim.env.PATH = vim.env.HOME .. '\\AppData\\Local\\mise\\shims;' .. vim.env.PATH
 else
