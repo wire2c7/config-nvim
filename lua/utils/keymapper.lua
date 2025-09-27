477dd483662294398b3f9d1b7b8ebe1054b06a4c
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-27 15:12:27 +0900

    feat: keymapperユーティリティに型情報を追加

--- lua/utils/keymapper.lua
+++ lua/utils/keymapper.lua
@@ -1,5 +1,7 @@
-local M = {}
+---@alias modes 'n'|'v'|'i'|'c'|'o'|'t'
+---@alias mapper fun(lhs: string, rhs: string|function, opts?: vim.keymap.set.Opts)
 
+---@ type modes[]
 local modes = {
   'n', -- Normal
   'v', -- Visual
@@ -9,18 +11,24 @@ local modes = {
   't', -- Terminal
 }
 
+---@type { [modes]: mapper }
+local M = {}
+
 local default_opts = { silent = true }
 
+---@param mode modes
+---@param lhs string
+---@param rhs string|function
+---@param opts? vim.keymap.set.Opts
 local function set(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, vim.tbl_deep_extend('force', default_opts, opts or {}))
 end
 
 for _, mode in pairs(modes) do
-  M[mode .. 'map'] = function(lhs, rhs, opts)
+  ---@type mapper
+  M[mode] = function(lhs, rhs, opts)
     set(mode, lhs, rhs, opts)
   end
 end
-
-M['map'] = set
 
 return M
