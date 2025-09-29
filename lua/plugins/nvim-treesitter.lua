af5eeb3fa5b25904a263f089f32725c89318c1cd
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-30 00:41:58 +0900

    feat: nvim-treesitterの設定とインストールするパーサのリストを追加

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,18 +2,63 @@ local M = {
   'nvim-treesitter/nvim-treesitter',
   branch = 'main',
   event = { 'BufNewFile', 'BufReadPre' },
-  config = function()
-    require('nvim-treesitter').setup({})
-
-    vim.api.nvim_create_autocmd('FileType', {
-      group = vim.api.nvim_create_augroup('user__vim_treesitter_start', {}),
-      callback = function()
-        pcall(vim.treesitter.start)
-
-        vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-      end,
-    })
-  end,
+  build = ':TSUpdate',
+  lazy = false,
 }
+
+local languages = {
+  'bash',
+  'c_sharp',
+  'c',
+  'comment',
+  'cpp',
+  'css',
+  'csv',
+  'desktop',
+  'dockerfile',
+  'editorconfig',
+  'git_config',
+  'git_rebase',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+  'html',
+  'javascript',
+  'jq',
+  'jsdoc',
+  'json',
+  'jsonc',
+  'lua',
+  'luadoc',
+  'markdown_inline',
+  'markdown',
+  'powershell',
+  'regex',
+  'rust',
+  'ssh_config',
+  'toml',
+  'tsv',
+  'tsx',
+  'typescript',
+  'xml',
+  'yaml',
+  'zig',
+}
+
+function M.config()
+  local nvim_treesitter = require('nvim-treesitter')
+  nvim_treesitter.setup({})
+  nvim_treesitter.install(languages)
+
+  vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('user__vim_treesitter_start', {}),
+    callback = function()
+      pcall(vim.treesitter.start)
+
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+    end,
+  })
+end
 
 return { M }
